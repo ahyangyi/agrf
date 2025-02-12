2346f801224e4b13ce5153d2fbe6b4b38669a5f6
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-13 01:35:24 +0800

    Fix: do not multiply the base sprite alpha to the night mask

--- agrf/graphics/cv/nightmask.py
+++ agrf/graphics/cv/nightmask.py
@@ -9,7 +9,7 @@ def make_night_mask(img: LayeredImage, darkness=0.75) -> LayeredImage:
     if img.alpha is None:
         mask = ((img.mask > 0) * 255 * darkness).astype(np.uint8)
     else:
-        mask = (img.alpha * darkness).astype(np.uint8)
+        mask = ((img.alpha > 0) * 255 * darkness).astype(np.uint8)
 
     return LayeredImage(
         xofs=img.xofs,
