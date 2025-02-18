523998b84707f721e000d0c3df0c01ec4d9d8133
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-18 15:53:34 +0800

    (building) SUpport loading groudsprites with different height

--- agrf/lib/building/image_sprite.py
+++ agrf/lib/building/image_sprite.py
@@ -2,16 +2,16 @@ import grf
 from agrf.graphics import SCALE_TO_ZOOM
 
 
-def image_sprite(path, crop=True, child=False):
+def image_sprite(path, y=127, crop=True, child=False):
     return grf.AlternativeSprites(
         grf.FileSprite(
             grf.ImageFile(path),
             0,
             0,
             256,
-            127,
+            y,
             xofs=0 if child else -124,
-            yofs=0,
+            yofs=127 - y,
             bpp=32,
             zoom=SCALE_TO_ZOOM[4],
             crop=crop,
