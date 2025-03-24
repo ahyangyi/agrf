787247591cf91990c30a952e114eff53bd2329e6
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-03-25 06:40:59 +0800

    (building) set symmetry for the empty sprite

--- agrf/lib/building/default.py
+++ agrf/lib/building/default.py
@@ -1,5 +1,6 @@
 import grf
+from agrf.lib.building.symmetry import BuildingCylindrical
 from agrf.lib.building.layout import AGroundSprite
 from agrf.lib.building.registers import Registers
 
-empty_ground = AGroundSprite(grf.EMPTY_SPRITE, flags={"add": Registers.ZERO})
+empty_ground = BuildingCylindrical.create_variants([AGroundSprite(grf.EMPTY_SPRITE, flags={"add": Registers.ZERO})])
