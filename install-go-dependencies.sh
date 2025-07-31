20d4f76bbc7fd92d98c96ee6822eb817ed8e0576
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-08-01 01:09:09 +0800

    ...

--- install-go-dependencies.sh
+++ install-go-dependencies.sh
@@ -6,3 +6,4 @@ go install github.com/ahyangyi/cargopositor/cmd@f9051fa
 mv ${GOPATH}/bin/cmd ${GOPATH}/bin/positor
 go install github.com/ahyangyi/gandalf/cmd@v1.4.0-ah4
 mv ${GOPATH}/bin/cmd ${GOPATH}/bin/layer-filter
+strip ${GOPATH}/bin/*
