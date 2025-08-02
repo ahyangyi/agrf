905fcbf36e07e7ebd170f44ad06fd907cb8b89a6
Yi Yang <ahyangyi@gmail.com>
2025-08-03 03:54:05 +0800

    (gorender) Bundle binaries of Timberwolf tools (#6)

---
+++ install-go-dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export GOPATH=$PWD/agrf/gorender/
+go install github.com/ahyangyi/gorender/cmd@cdca513
+mv ${GOPATH}/bin/cmd ${GOPATH}/bin/gorender
+go install github.com/ahyangyi/cargopositor/cmd@f9051fa
+mv ${GOPATH}/bin/cmd ${GOPATH}/bin/positor
+go install github.com/ahyangyi/gandalf/cmd@v1.4.0-ah4
+mv ${GOPATH}/bin/cmd ${GOPATH}/bin/layer-filter
+strip ${GOPATH}/bin/*
