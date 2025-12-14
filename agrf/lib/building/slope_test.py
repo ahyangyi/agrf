889bb8f634ba68e6182b60f52c35eeee672321db
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-12-14 18:39:53 +0800

    (building) fix the test cases I just broke

--- agrf/lib/building/slope_test.py
+++ agrf/lib/building/slope_test.py
@@ -9,33 +9,37 @@ class MockObject:
 
 
 def test_cylindrical():
-    sprites = {i: BuildingCylindrical.create_variants([MockObject(i)]) for i in slope_types}
+    sprites = {i.value: BuildingCylindrical.create_variants([MockObject(i.offset)]) for i in slope_types}
     ret = make_slopes(sprites, BuildingCylindrical)
 
     for i in slope_types:
-        assert ret[0][i] is sprites[i]
+        assert ret[0][i.value] is sprites[i.value]
 
 
 def test_x():
-    sprites = {i: BuildingSymmetricalX.create_variants([MockObject(i * 4 + j) for j in range(4)]) for i in slope_types}
+    sprites = {
+        i.value: BuildingSymmetricalX.create_variants([MockObject(i.offset * 4 + j) for j in range(4)])
+        for i in slope_types
+    }
     ret = make_slopes(sprites, BuildingSymmetricalX)
 
     for i in slope_types:
-        assert ret[0][i] is sprites[i]
+        assert ret[0][i.value] is sprites[i.value]
         for j in [1, 4, 5]:
-            print(i, j)
-            assert i in ret[j]
+            assert i.value in ret[j]
     assert ret[1][0] is sprites[0].M
 
 
 def test_full():
-    sprites = {i: BuildingFull.create_variants([MockObject(i * 8 + j) for j in range(8)]) for i in slope_types}
+    sprites = {
+        i.value: BuildingFull.create_variants([MockObject(i.offset * 8 + j) for j in range(8)]) for i in slope_types
+    }
     ret = make_slopes(sprites, BuildingFull)
 
     for i in slope_types:
-        assert ret[0][i] is sprites[i]
+        assert ret[0][i.value] is sprites[i.value]
         for j in range(1, 8):
-            assert i in ret[j]
+            assert i.value in ret[j]
     assert ret[1][1] is sprites[4].M
     assert ret[2][3] is sprites[12].R
     assert ret[1][3] is sprites[6].M
