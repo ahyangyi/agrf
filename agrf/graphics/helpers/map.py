6d9dbeada933aa7feeb3227636acba190b154e51
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-12-24 02:27:00 +0800

    (graphics) multivariate map

--- agrf/graphics/helpers/map.py
+++ agrf/graphics/helpers/map.py
@@ -10,17 +10,20 @@ class MapAlternativeSprites(grf.AlternativeSprites):
         self.source = source
 
     def get_fingerprint(self):
-        return {"map-name": self.name, "source": self.source.get_fingerprint()}
+        return {"map-name": self.name, "source": [x.get_fingerprint() for x in self.source]}
 
 
 def map_alternative_sprites(a, f, name, xofs=0, yofs=0):
+    if not isinstance(a, tuple):
+        a = (a,)
     alts = []
     for scale in [1, 2, 4]:
         for bpp in [8, 32]:
-            if (sa := a.get_sprite(zoom=SCALE_TO_ZOOM[scale], bpp=bpp)) is None:
+            sa = tuple(b.get_sprite(zoom=SCALE_TO_ZOOM[scale], bpp=bpp) for b in a)
+            if all(x is None for x in sa):
                 continue
             fs = MapSprite(
-                sa, lambda x, scale=scale, bpp=bpp: f(x, scale, bpp), name, xofs=xofs * scale, yofs=yofs * scale
+                sa, (lambda x, scale=scale, bpp=bpp: f(x, scale, bpp)), name, xofs=xofs * scale, yofs=yofs * scale
             )
             alts.append(fs)
 
